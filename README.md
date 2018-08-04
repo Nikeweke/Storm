a5b3cefa613e0dc116de60872eccd2a4594e536b
Nikeweke <jxammet@gmail.com>
2018-08-04 14:39:29 +0300

    new files

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## Storm - brother of Bullz
 Fast and reliable app to make problem runs
 
+### Quick start
+```bash
+# Install packages
+.\packages # windows
+# OR 
+./packages
+
+# Run app
+./runner
+```
--- packages.bat
+++ packages.bat
@@ -5,9 +5,8 @@ chcp 866>nul
 SET GOPATH=%CD%
 SET PATH=%PATH%;%GOPATH%\BIN;
 
-REM go get github.com/pilu/fresh
-REM go get -u github.com/gorilla/mux
-REM go get github.com/fatih/color
+go get github.com/pilu/fresh
+go get -u github.com/gorilla/mux
+go get github.com/fatih/color
 
 
-REM go get -u github.com/logrusorgru/aurora
