13e74664b560b195ebceb6a7f1c46588edeaa3ff
Nikeweke <jxammet@gmail.com>
2018-08-09 22:47:12 +0300

    new scripts

---
+++ build.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SETLOCAL
+chcp 866>nul
+
+SET GOPATH=%CD%
+SET PATH=%PATH%;%GOPATH%\BIN;
+
+REM Linux Build settings
+REM SET GOOS=linux
+REM SET GOARCH=amd64
+REM SET CGO_ENABLED=0
+
+go build main.go
+REM pause
--- packages.bat
+++ packages.bat
@@ -10,7 +10,6 @@ REM go get github.com/pilu/fresh
 
 REM go get github.com/fatih/color
 REM go get -u github.com/gin-gonic/gin
-
 REM go get -u github.com/gorilla/mux
 REM go get github.com/gorilla/websocket
 REM go get github.com/robfig/cron
